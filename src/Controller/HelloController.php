c481e7f7e177c7741530078440aabfa0faefbc1f
root <root@ip-172-31-24-253.ec2.internal>
2022-10-15 18:42:33 +0000

    Add logger and HelloController

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Controller;
+
+use App\Service\RequestValidator\RequestValidator;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Psr\Log\LoggerInterface;
+
+
+class HelloController extends AbstractController
+{
+    private $logger;
+    public function __construct(LoggerInterface $logger)
+    {
+       
+        $this->logger = $logger;
+    }
+    public function __invoke(Request $request, LoggerInterface $logger): Response
+    {
+        $this->logger->info("ENTRO");
+        return new JsonResponse("",Response::HTTP_OK);
+    }
+
+}
