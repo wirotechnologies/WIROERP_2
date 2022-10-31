5d973ed9cb727143e20992506b36b8b291a25a67
Angelica <avalderrama@consured.com>
2022-10-31 16:41:03 -0500

    change datajson as requestm parameter

--- src/Service/RequestValidator/RequestValidator.php
+++ src/Service/RequestValidator/RequestValidator.php
@@ -18,7 +18,8 @@ class RequestValidator
         //dd($dataJson);
         $uploadFileEnergyInvoice = $request->files->get('fileEnergyInvoice') ?? throw new BadRequestHttpException('400 File Energy Invoice not upload', null, 400);
         $uploadFileIdentificationDocument = $request->files->get('identificationDocument') ?? throw new BadRequestHttpException('400 File Identification Document not upload', null, 400);
-        $customerId =  $dataJson['identification']["value"] ?? throw new BadRequestHttpException('400', null, 400);
+        $customerId =  $dataJson['identification']["value"];
+        // ?? throw new BadRequestHttpException('400', null, 400);
         $customerType =  $dataJson['customerType'] ?? throw new BadRequestHttpException('400', null, 400);
         $customerIdentifierType =  $dataJson['identification']['idIdentifierType'] ?? throw new BadRequestHttpException('400', null, 400);
         $email = $dataJson['email'] ?? throw new BadRequestHttpException('400', null, 400);
